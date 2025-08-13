92fb605f3b9b62b38b54cb030f9f9619d778f5b7
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-13 15:57:33 +0700

    update

--- image-review-app/image-review-frontend/src/components/ImageList.jsx
+++ image-review-app/image-review-frontend/src/components/ImageList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function ImageList() {
   const [images, setImages] = useState([]);
+  const [commentInputs, setCommentInputs] = useState({});
 
   useEffect(() => {
     fetch("http://localhost:9999/api/images")
@@ -17,6 +18,22 @@ function ImageList() {
       .then(setImages);
   };
 
+  const handleComment = (id) => {
+    const text = commentInputs[id];
+    if (!text) return;
+    fetch(`http://localhost:9999/api/images/${id}/comment`, {
+      method: "POST",
+      headers: { "Content-Type": "Application/json" },
+      body: JSON.stringify({ text }),
+    });
+
+    setCommentInputs({ ...commentInputs, [id]: "" });
+
+    fetch("http://localhost:9999/api/images")
+      .then((res) => res.json())
+      .then(setImages);
+  };
+
   return (
     <div>
       <h1>Gallery</h1>
@@ -24,6 +41,21 @@ function ImageList() {
         <div key={i._id}>
           <img src={i.secure_url} alt={i.originalname} width={200} />
           <button onClick={() => handleLike(i._id)}>Like {i.likes}</button>
+          Comments:
+          <ul>
+            {i.comments.map((cmt, idx) => (
+              <li key={idx}>{cmt.text}</li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            value={commentInputs[i._id] || ""}
+            onChange={(e) =>
+              setCommentInputs({ ...commentInputs, [i._id]: e.target.value })
+            }
+            placeholder="What do you think?"
+          />
+          <button onClick={() => handleComment(i._id)}>Send</button>
         </div>
       ))}
     </div>
